1eb4cea30eab4b50d3f0a581c29d159bfdb0f32b
Leonel Elimpe <leonelngande@gmail.com>
2020-08-18 15:13:54 +0100

    Add privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'about',
     loadChildren: './about/about.module#AboutPageModule'
+  },
+  {
+    path: 'privacy',
+    loadChildren: () => import('./privacy/privacy.module').then( m => m.PrivacyPageModule)
   }
 ];
 
